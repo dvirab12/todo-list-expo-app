5737ab624a594cc2d6fbd61a83becb40911e3ff3
dvirab12 <dvirab12@gmail.com>
2024-08-22 20:25:06 +0300

    BE changes

--- BE/App.js
+++ BE/App.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;  
 
+const tasksRouter = require("./routers/tasks")
+
 app.use(express.json());
+app.use('/tasks', tasksRouter);
 
 const users = [{username: "admin", password: "admin"}]
 
---
+++ BE/routers/tasks.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+tasks = []
+
+router.get('/', (req, res) => {
+    res.json(tasks);
+})
+
+router.get('/:id', (req, res) => {
+    const taskId = req.params.id;
+    const task = Tasks.find(tasks => tasks.id === id);
+
+    if (task) {
+        res.status(200).json(task);
+    } else {
+        res.status(401).json({message: "taskID not found!"});
+    }
+})
+
+router.post('/', (req, res) => {
+    const {title, description} = req.body;
+
+    if (!title) {
+        return res.status(400).send("ERROR: Title is required!")
+    }
+
+    const newTask = {
+        id: tasks.lengyh + 1,
+        title: title,
+        description: description || ""
+    }
+
+    tasks.push(newTask);
+    res.status(201).send("Added new task!");
+})
+
+router.delete('/:id', (req, res) => {
+    const taskID = req.params.id;
+    const task = Tasks.find(tasks => tasks.id === id);
+
+    if (task) {
+        tasks.splice(taskID - 1, 1)
+        res.status(200).json({message: `Deleted task ${taskID}`});
+    } else {
+        res.status(401).json({message: "taskID not found!"});
+    }
+})
+
+module.exports = router;
