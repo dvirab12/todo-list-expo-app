e6e091a02636a14f62714a7448f2174322bfabbd
Dvirab12 <dvirab12@gmail.com>
2024-08-31 00:45:47 +0300

    fix: fixed authentication middleware

--- BE/routers/tasks.js
+++ BE/routers/tasks.js
@@ -6,15 +6,24 @@ const jwt = require('jsonwebtoken');
 
 //auth mmiddleware
 const authenticate = (req, res, next) => {
-    const token = req.headers['Authorization'];
+    const authHeader = req.headers['authorization']; 
+    console.log('Authorization Header:', authHeader);
 
-    if(!token) return res.status(401).send("Access denied. No token provided.");
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(403).send('Invalid token');
+    const token =authHeader.split(' ')[1];
+    console.log('token:', token);
+
+    if (!token) {
+        return res.status(401).send("Access denied. No token provided.");
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send('Invalid token');
+        }
         req.userId = decoded.id;
         next();
     });
-}
+};
 
 // Get all tasks
 router.get('/', authenticate, async (req, res) => {
